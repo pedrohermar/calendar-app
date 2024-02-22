f45b5ae464281e89c1d6377aabb5199cdc25ff6b
Pedro <pedrohm92@gmail.com>
2024-02-22 14:22:07 +0100

    Configurada carga de eventos desde la bd

--- src/hooks/useCalendarStore.js
+++ src/hooks/useCalendarStore.js
@@ -1,11 +1,14 @@
 import { useDispatch, useSelector } from "react-redux";
-import { onAddNewEvent, onDeleteEvent, onSetActiveEvent, onUpdateEvent } from "../store";
+import { onAddNewEvent, onDeleteEvent, onLoadEvents, onSetActiveEvent, onUpdateEvent } from "../store";
+import calendarApi from "../api/calendarApi";
+import { convertEventsToDateEvents } from "../helpers";
 
 export const useCalendarStore = () => {
     const dispatch = useDispatch()
   const { events, activeEvent } = useSelector((state) => state.calendar);
+  const { user } = useSelector((state) => state.auth);
 
-  const setActiveEvent = ( calendarEvent ) => {
+  const setActiveEvent = async( calendarEvent ) => {
     dispatch( onSetActiveEvent( calendarEvent ) )
   }
 
@@ -17,13 +20,29 @@ export const useCalendarStore = () => {
         dispatch( onUpdateEvent( calendarEvent ))
     } else {
         // Creando
-        dispatch( onAddNewEvent({ ...calendarEvent, _id: new Date().getTime() }) )
+        const { data } = await calendarApi.post('/events', calendarEvent)
+        console.log(data)
+        dispatch( onAddNewEvent({ ...calendarEvent, id: data.event.id, user }) )
     }
   }
 
   const startDeletingEvent = async() => {
     dispatch( onDeleteEvent() )
   }
+
+  const startLoadingEvents = async() => {
+    try {
+
+      const { data } = await calendarApi.get('/events')
+      const events = convertEventsToDateEvents( data.events )
+      dispatch( onLoadEvents(events) )
+      
+    } catch (error) {
+      console.log('Error cargando los eventos')
+      console.log(error)
+    }
+  }
+
 
   return {
     // Propiedades
@@ -34,6 +53,7 @@ export const useCalendarStore = () => {
     //Métidos
     setActiveEvent,
     startSavingEvent, 
-    startDeletingEvent
+    startDeletingEvent,
+    startLoadingEvents
   };
 };
--- src/store/calendar/calendarSlice.js
+++ src/store/calendar/calendarSlice.js
@@ -1,23 +1,26 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { addHours } from 'date-fns';
 
-const tempEvent = {
-    _id: new Date().getTime(),
-    title: "Evento de prueba",
-    notes: "Comprar tarta",
-    start: new Date(),
-    end: addHours(new Date(), 2),
-    bgColor: "#fafafa",
-    user: {
-      _id: "123",
-      name: "Pedro",
-    },
-  }
+// const tempEvent = {
+//     _id: new Date().getTime(),
+//     title: "Evento de prueba",
+//     notes: "Comprar tarta",
+//     start: new Date(),
+//     end: addHours(new Date(), 2),
+//     bgColor: "#fafafa",
+//     user: {
+//       _id: "123",
+//       name: "Pedro",
+//     },
+//   }
 
 export const calendarSlice = createSlice({
     name: 'calendar',
     initialState: {
-        events: [ tempEvent ],
+        isLoadingEvents: true,
+        events: [ 
+            // tempEvent 
+        ],
         activeEvent: null
     },
     reducers: {
@@ -42,9 +45,18 @@ export const calendarSlice = createSlice({
                 state.events = state.events.filter( event => event._id !== state.activeEvent._id )
                 state.activeEvent = null
             }
+        },
+        onLoadEvents: (state, { payload = [] }) => {
+            state.isLoadingEvents = false
+            payload.forEach( event => {
+                const exist = state.events.some( dbEvent => dbEvent.id === event.id )
+                if(!exist) {
+                    state.events.push( event )
+                }
+            })
         }
     }
 });
 
 
-export const { onSetActiveEvent, onAddNewEvent, onUpdateEvent, onDeleteEvent } = calendarSlice.actions;
+export const { onSetActiveEvent, onAddNewEvent, onUpdateEvent, onDeleteEvent, onLoadEvents } = calendarSlice.actions;
