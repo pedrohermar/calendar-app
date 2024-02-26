8c857ef70012c33a2faf3bf6d9d98be2c22d765d
Pedro <pedrohm92@gmail.com>
2024-02-26 17:55:22 +0100

    Color de evento condicional

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -5,9 +5,10 @@ import "react-big-calendar/lib/css/react-big-calendar.css";
 import { CalendarEvent, CalendarModal, FabAddNew, FabDelete, Navbar } from "../";
 
 import { localizer, getMessagesES } from "../../helpers";
-import { useCalendarStore, useUiStore } from "../../hooks";
+import { useAuthStore, useCalendarStore, useUiStore } from "../../hooks";
 
 export const CalendarPage = () => {
+  const { user } = useAuthStore()
   const { openDateModal } = useUiStore();
   const { events, setActiveEvent, startLoadingEvents } = useCalendarStore();
 
@@ -16,8 +17,11 @@ export const CalendarPage = () => {
   );
 
   const eventStyleGetter = (event, start, end, isSelected) => {
+
+    const isMyEvent = ( user.uid === event.user._id ) || ( user.uid === event.user.uid )
+
     const style = {
-      backgroundColor: "#347CF7",
+      backgroundColor: isMyEvent ? "#347CF7" : '#465660',
       borderRadius: "0px",
       opacity: 0.8,
       color: "white",
