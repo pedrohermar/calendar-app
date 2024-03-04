ab83a2e70831c494cad47a54d49cd5c5ce649858
Pedro <pedrohm92@gmail.com>
2024-03-04 18:08:42 +0100

    Preparación entorno de pruebas

---
+++ jest.setup.js
@@ -0,0 +1,18 @@
+// En caso de necesitar la implementación del FetchAPI
+// npm i -D whatwg-fetch
+// import 'whatwg-fetch'; 
+
+// En caso de encontrar paquetes que lo requieran 
+// npm i -D setimmediate
+// import 'setimmediate';
+
+// En caso de tener variables de entorno y aún no soporta el import.meta.env
+// npm i -D dotenv
+require('dotenv').config({
+    path: '.env.test'
+});
+
+// Realizar el mock completo de las variables de entorno
+jest.mock('./src/helpers/getEnv', () => ({
+    getEnvVariables: () => ({ ...process.env })
+}));
